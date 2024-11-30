01cc8d3d08a9e706b25c5ec556dbd180a0ebbe9a
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-11-29 21:05:26 -0400

    feat: create Order and User entities with Enum

--- src/main/java/com/viniciuslima/dscommerce/DscommerceApplication.java
+++ src/main/java/com/viniciuslima/dscommerce/DscommerceApplication.java
@@ -8,7 +8,7 @@ public class DscommerceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DscommerceApplication.class, args);
-        System.out.println("teste");
+        System.out.println("PROJETO DS-COMMERCE INICIADO...\uD83D\uDE80 \uD83D\uDE80 \uD83D\uDE80");
     }
 
 }
---
+++ src/main/java/com/viniciuslima/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.viniciuslima.dscommerce.entities;
+
+public enum OrderStatus {
+    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+}
