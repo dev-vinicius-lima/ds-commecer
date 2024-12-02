c6a00ecf524d5966316d9b03c7c32b19c9f2b309
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-12-02 15:09:12 -0400

    feat: :sparkles: get products pageable

--- src/main/java/com/viniciuslima/dscommerce/services/ProductService.java
+++ src/main/java/com/viniciuslima/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.viniciuslima.dscommerce.dto.ProductDTO;
 import com.viniciuslima.dscommerce.entities.Product;
 import com.viniciuslima.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,5 +21,11 @@ public class ProductService {
     public ProductDTO findById(Long id) {
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(ProductDTO::new);
     }
 }
