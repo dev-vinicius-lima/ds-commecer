80cd5983c623566d2d4275a54ece28c09845a2fd
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-12-28 17:30:02 -0400

    feat: :sparkles: Domain model User, Role and seed

--- src/main/java/com/viniciuslima/dscommerce/entities/User.java
+++ src/main/java/com/viniciuslima/dscommerce/entities/User.java
@@ -3,9 +3,7 @@ package com.viniciuslima.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
@@ -24,6 +22,10 @@ public class User {
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
 
+    @ManyToMany
+    @JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
     public User() {
     }
 
@@ -35,6 +37,7 @@ public class User {
         this.password = password;
         this.phone = phone;
     }
+
 
     public LocalDate getBirthDate() {
         return birthDate;
@@ -86,6 +89,23 @@ public class User {
 
     public List<Order> getOrders() {
         return orders;
+    }
+
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public boolean hasRole(String roleName) {
+        for (Role role : roles) {
+            if (role.getAuthority().equals(roleName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
     }
 
     @Override
